90e6fe4b23452f295df7231622c491b7632c3efb
eunzzzzz1 <eunzzzzz1@kakao.com>
2024-05-06 16:50:34 +0900

    :tada: first upload

---
+++ src/main/java/com/byeolbook/byeolbook/ByeolbookApplication.java
@@ -0,0 +1,13 @@
+package com.byeolbook.byeolbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByeolbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByeolbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/byeolbook/byeolbook/ByeolbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byeolbook.byeolbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByeolbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
